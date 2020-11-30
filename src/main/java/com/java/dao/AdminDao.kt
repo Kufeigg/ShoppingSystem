4c23fc60c7d96b6e94bfddc7c73b148feef55f7c
lvyangai <1076977275@qq.com>
2020-11-30 10:36:43 +0800

    dao层

--- src/main/java/com/java/dao/AdminDao.kt
+++ src/main/java/com/java/dao/AdminDao.kt
@@ -3,13 +3,6 @@ package com.java.dao
 import com.java.pojo.Auser
 import org.apache.ibatis.annotations.Param
 
-/**
- * @Description
- * @Author sc <1501482176@qq.com>
- * @Version V1.0.0
- * @Since 1.0
- * @Date 2020/10/1
- */
 interface AdminDao {
     /**
      * 登陆
--- src/main/java/com/java/dao/GoodsDao.kt
+++ src/main/java/com/java/dao/GoodsDao.kt
@@ -5,13 +5,7 @@ import com.java.vo.GoodVo
 import org.apache.ibatis.annotations.Param
 import sun.jvm.hotspot.gc_interface.GCName
 
-/**
- * @Description
- * @Author sc <1501482176@qq.com>
- * @Version V1.0.0
- * @Since 1.0
- * @Date 2020/10/10
- */
+
 interface GoodsDao {
     fun findGoodsByPage(goodVo: GoodVo?): List<Goodstable?>?
     fun findGoodsByName(@Param("gName") gcName: String?):List<Goodstable?>?
